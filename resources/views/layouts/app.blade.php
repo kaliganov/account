c0cdb2424d497265439ed36680b8e5f382db6ee5
kaliganov <kaliganovss@yandex.ru>
2026-03-27 18:33:10 +0300

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 <body class="bg-light">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand me-5" href="{{ route('home') }}">СчетOn</a>
+        <a class="navbar-brand me-5" href="{{ route('home') }}">Тест</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNav" aria-controls="topNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
